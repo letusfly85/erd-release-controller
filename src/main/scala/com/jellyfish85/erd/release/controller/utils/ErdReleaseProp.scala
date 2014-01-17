487cfd5a0d3fb5bcffe7149c7a22a03c79a461e0
letusfly85 <letusfly95@gmail.com>
2014-01-18 00:39:26 +0900

    modify build.gradle file

--- src/main/scala/com/jellyfish85/erd/release/controller/utils/ErdReleaseProp.scala
+++ src/main/scala/com/jellyfish85/erd/release/controller/utils/ErdReleaseProp.scala
@@ -1,12 +1,21 @@
 package com.jellyfish85.erd.release.controller.utils
 
+import org.apache.commons.configuration.{PropertiesConfiguration}
+import java.io.InputStream
+
 /**
  * == ErdReleaseProp ==
  *
  *
  */
-class ErdReleaseProp {
+class ErdReleaseProp() {
 
+  val inputStream: InputStream =
+    getClass.getResourceAsStream("/com/jellyfish85/erd/release/controller/define/erd.release.control.properties")
 
+  val configuration: PropertiesConfiguration =
+    new PropertiesConfiguration()
+
+  configuration.load(inputStream, "UTF8")
 
 }
