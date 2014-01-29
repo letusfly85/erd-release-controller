0ffcab09cbba4ed3bc44c98ceac584387e06fba3
letusfly85 <letusfly95@gmail.com>
2014-01-29 23:13:31 +0900

    add validate duplicate records method

--- src/main/groovy/com/jellyfish85/erd/release/controller/validator/RrTabDefInfoValidator.groovy
+++ src/main/groovy/com/jellyfish85/erd/release/controller/validator/RrTabDefInfoValidator.groovy
@@ -1,9 +1,44 @@
 package com.jellyfish85.erd.release.controller.validator
 
+import com.jellyfish85.dbaccessor.bean.erd.mainte.tool.RrTabDefInfoBean
+import com.jellyfish85.erd.release.controller.bean.RrTabDefInfoValidateBean
+
 class RrTabDefInfoValidator extends GeneralValidator {
+
+    private ArrayList<RrTabDefInfoValidateBean> validateBeans = null
 
     public RrTabDefInfoValidator() {
         super()
+
+        validateBeans = new ArrayList<>()
     }
 
+    /**
+     * validate RR_TAB_DEF_INFO duplication
+     *
+     *
+     * @param beans
+     */
+    public void validateDuplicateRecords(ArrayList<RrTabDefInfoBean> beans) {
+        HashMap<String, String> duplicateMap = new HashMap<>()
+
+        beans.eachWithIndex {RrTabDefInfoBean bean, int index ->
+
+            beans.eachWithIndex {RrTabDefInfoBean _bean, int _index ->
+                if (!index.equals(_index) &&
+                        bean.physicalColumnNameAttr().value_$eq(_bean.physicalColumnNameAttr().value())) {
+                    duplicateMap.put(bean.physicalColumnNameAttr().value(), "")
+                }
+            }
+        }
+
+        if (duplicateMap.size() > 0) {
+            duplicateMap.each {key, value ->
+                RrTabDefInfoValidateBean validateBean = new RrTabDefInfoValidator()
+                validateBean.setResults(false, key)
+
+                validateBeans.add(validateBean)
+            }
+        }
+    }
 }
